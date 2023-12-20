d49c606bbc0a66ca3a8fdcd169dbce229a23df82
Faizan Ali <151016446+faizan1236@users.noreply.github.com>
2023-12-19 23:51:14 -0800

    new changing

--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -9,19 +9,19 @@ const Card = () =>{
 <img src="Frame.png" alt="" />
 <p class="font-normal text-gray-700 dark:text-gray-400 lg:pt-[1rem]"><b>2023</b>/ Case Study</p>
 <p className="font-[600] text-[1.5rem] lg:pt-[1rem] leading-[27px]">Skip the bank, borrow from those you trust</p>
-<p className="text-[0.9rem] text-[1rem] lg:pt-[0.5rem] text-[#515151] leading-[20.8px] font-[300]">FamilyFund is a crowdfunding platform for friends and family. Allowing users to take personal loans from their network without a traditional financial institution.</p>
+<p className="text-[13px] text-[1rem] lg:pt-[0.5rem] text-[#515151] leading-[20.8px] font-[300]">FamilyFund is a crowdfunding platform for friends and family. Allowing users to take personal loans from their network without a traditional financial institution.</p>
 </div>
 <div data-aos="flip-up" class="block max-w-full p-8 bg-white border border-gray-200 rounded-lg mt-[10px] mx-[1rem] lg:mx-[0rem] shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
 <img src="one.png" alt="" />
 <p class="font-normal text-gray-700 dark:text-gray-400 lg:pt-[1rem] "><b>2022</b>/ Case Study</p>
 <p className="font-bold text-[1.4rem] lg:pt-[0.7rem] leading-[27px] font-[600]">Get a hodl of your health</p>
-<p className="text-[0.9rem] text-[1rem] lg:pt-[0.5rem] text-[#515151] leading-[20.8px] font-[300]">Unseal is the first NFT platform where users can mint and trade NFTs of their own personal health records, allowing them to take control of their data.</p>
+<p className="text-[13px] text-[1rem] lg:pt-[0.5rem] text-[#515151] leading-[20.8px] font-[300]">Unseal is the first NFT platform where users can mint and trade NFTs of their own personal health records, allowing them to take control of their data.</p>
 </div>
 <div data-aos="flip-up" class="block max-w-full p-6 bg-white border border-gray-200 rounded-lg mt-[10px] mx-[1rem] lg:mx-[0rem] shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
 <img src="Frame.png" alt="" />
 <p class="font-normal text-gray-700 dark:text-gray-400 lg:pt-[1rem]"><b>2023</b>/ Case Study</p>
 <p className=" text-[1.5rem] leading-[27.6px] lg:pt-[1rem] font-[600]">Overcome your fears, find your match</p>
-<p className="text-[0.9rem] lg:pt-[0.5rem] text-[#515151] leading-[20.8px] font-[300]">Find love in the face of fear — Phobia is a dating app that matches users based on their mutual phobias so they can be scared together.</p>
+<p className="text-[14px] lg:pt-[0.5rem] text-[#515151] leading-[20.8px] font-[300]">Find love in the face of fear — Phobia is a dating app that matches users based on their mutual phobias so they can be scared together.</p>
 </div>
 
 
